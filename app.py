7d6ae51398d52c49135e802a1bf1b8360fc16187
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-08-26 23:09:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load sentiment analysis pipeline
+sentiment_analyzer = pipeline("sentiment-analysis")
+
+# Streamlit UI
+st.title("Sentiment Analysis App")
+text = st.text_area("Enter text for sentiment analysis:")
+if st.button("Analyze"):
+    if text:
+        results = sentiment_analyzer(text)
+        sentiment = results[0]['label']
+        score = results[0]['score']
+        st.write(f"Sentiment: {sentiment}")
+        st.write(f"Confidence Score: {score:.2f}")
